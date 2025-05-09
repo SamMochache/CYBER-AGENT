3ea320d39b086c4cc9bfbb64c4e6c534d7007fec
SamMochache <sammochache01@gmail.com>
2025-05-10 02:09:53 +0300

    add Home page with navigation and background image, update App component for routing, and include react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+  
 }
 
 export default App;
